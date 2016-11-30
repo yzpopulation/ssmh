aaa544e2c3532cd5b54a5467b691d2959bb9db55
yy <yzpopulation@hotmail.com>
2016-11-30 09:04:26 +0800

    no message

---
+++ ssmh/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace ssmh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.modernTextBox1 = new DevLib.ModernUI.Forms.ModernTextBox();
+            this.modernButton1 = new DevLib.ModernUI.Forms.ModernButton();
+            this.modernTextBox2 = new DevLib.ModernUI.Forms.ModernTextBox();
+            this.modernButton2 = new DevLib.ModernUI.Forms.ModernButton();
+            this.modernDataGridView1 = new DevLib.ModernUI.Forms.ModernDataGridView();
+            this.modernButton3 = new DevLib.ModernUI.Forms.ModernButton();
+            ((System.ComponentModel.ISupportInitialize)(this.modernDataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.Location = new System.Drawing.Point(1, 377);
+            this.StatusStrip.Size = new System.Drawing.Size(645, 20);
+            // 
+            // modernTextBox1
+            // 
+            this.modernTextBox1.Lines = new string[0];
+            this.modernTextBox1.Location = new System.Drawing.Point(23, 63);
+            this.modernTextBox1.MaxLength = 2147483647;
+            this.modernTextBox1.Name = "modernTextBox1";
+            this.modernTextBox1.PasswordChar = '\0';
+            this.modernTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.modernTextBox1.SelectedText = "";
+            this.modernTextBox1.Size = new System.Drawing.Size(433, 23);
+            this.modernTextBox1.TabIndex = 0;
+            this.modernTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.modernTextBox1.UseSelectable = true;
+            this.modernTextBox1.UseStyleColors = false;
+            this.modernTextBox1.UseSystemPasswordChar = false;
+            this.modernTextBox1.WordWrap = true;
+            this.modernTextBox1.Click += new System.EventHandler(this.modernTextBox1_Click);
+            // 
+            // modernButton1
+            // 
+            this.modernButton1.Location = new System.Drawing.Point(462, 63);
+            this.modernButton1.Name = "modernButton1";
+            this.modernButton1.Size = new System.Drawing.Size(75, 23);
+            this.modernButton1.TabIndex = 1;
+            this.modernButton1.Text = "刷新";
+            this.modernButton1.UseSelectable = true;
+            this.modernButton1.UseStyleColors = false;
+            this.modernButton1.UseVisualStyleBackColor = true;
+            this.modernButton1.Click += new System.EventHandler(this.modernButton1_Click);
+            // 
+            // modernTextBox2
+            // 
+            this.modernTextBox2.Enabled = false;
+            this.modernTextBox2.Lines = new string[0];
+            this.modernTextBox2.Location = new System.Drawing.Point(23, 92);
+            this.modernTextBox2.MaxLength = 2147483647;
+            this.modernTextBox2.Name = "modernTextBox2";
+            this.modernTextBox2.PasswordChar = '\0';
+            this.modernTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.modernTextBox2.SelectedText = "";
+            this.modernTextBox2.Size = new System.Drawing.Size(514, 23);
+            this.modernTextBox2.TabIndex = 0;
+            this.modernTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.modernTextBox2.UseSelectable = true;
+            this.modernTextBox2.UseStyleColors = false;
+            this.modernTextBox2.UseSystemPasswordChar = false;
+            this.modernTextBox2.WordWrap = true;
+            // 
+            // modernButton2
+            // 
+            this.modernButton2.Location = new System.Drawing.Point(554, 92);
+            this.modernButton2.Name = "modernButton2";
+            this.modernButton2.Size = new System.Drawing.Size(75, 23);
+            this.modernButton2.TabIndex = 1;
+            this.modernButton2.Text = "浏览";
+            this.modernButton2.UseSelectable = true;
+            this.modernButton2.UseStyleColors = false;
+            this.modernButton2.UseVisualStyleBackColor = true;
+            this.modernButton2.Click += new System.EventHandler(this.modernButton2_Click);
+            // 
+            // modernDataGridView1
+            // 
+            this.modernDataGridView1.AllowUserToAddRows = false;
+            this.modernDataGridView1.AllowUserToDeleteRows = false;
+            this.modernDataGridView1.AllowUserToResizeRows = false;
+            this.modernDataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.modernDataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.modernDataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.modernDataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.modernDataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.modernDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.modernDataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.modernDataGridView1.EnableHeadersVisualStyles = false;
+            this.modernDataGridView1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.modernDataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.modernDataGridView1.Location = new System.Drawing.Point(23, 121);
+            this.modernDataGridView1.Name = "modernDataGridView1";
+            this.modernDataGridView1.ReadOnly = true;
+            this.modernDataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.modernDataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.modernDataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.modernDataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.modernDataGridView1.RowTemplate.Height = 23;
+            this.modernDataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.modernDataGridView1.Size = new System.Drawing.Size(606, 240);
+            this.modernDataGridView1.TabIndex = 2;
+            // 
+            // modernButton3
+            // 
+            this.modernButton3.Location = new System.Drawing.Point(555, 63);
+            this.modernButton3.Name = "modernButton3";
+            this.modernButton3.Size = new System.Drawing.Size(75, 23);
+            this.modernButton3.TabIndex = 1;
+            this.modernButton3.Text = "下载";
+            this.modernButton3.UseSelectable = true;
+            this.modernButton3.UseStyleColors = false;
+            this.modernButton3.UseVisualStyleBackColor = true;
+            this.modernButton3.Click += new System.EventHandler(this.modernButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 384);
+            this.Controls.Add(this.modernDataGridView1);
+            this.Controls.Add(this.modernButton3);
+            this.Controls.Add(this.modernButton2);
+            this.Controls.Add(this.modernButton1);
+            this.Controls.Add(this.modernTextBox2);
+            this.Controls.Add(this.modernTextBox1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Form1";
+            this.ShowStatusStrip = true;
+            this.Text = "wnacg";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Controls.SetChildIndex(this.modernTextBox1, 0);
+            this.Controls.SetChildIndex(this.modernTextBox2, 0);
+            this.Controls.SetChildIndex(this.StatusStrip, 0);
+            this.Controls.SetChildIndex(this.modernButton1, 0);
+            this.Controls.SetChildIndex(this.modernButton2, 0);
+            this.Controls.SetChildIndex(this.modernButton3, 0);
+            this.Controls.SetChildIndex(this.modernDataGridView1, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.modernDataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevLib.ModernUI.Forms.ModernTextBox modernTextBox1;
+        private DevLib.ModernUI.Forms.ModernButton modernButton1;
+        private DevLib.ModernUI.Forms.ModernTextBox modernTextBox2;
+        private DevLib.ModernUI.Forms.ModernButton modernButton2;
+        private DevLib.ModernUI.Forms.ModernDataGridView modernDataGridView1;
+        private DevLib.ModernUI.Forms.ModernButton modernButton3;
+    }
+}
+
---
+++ ssmh/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevLib.ExtensionMethods;
+using DevLib.ModernUI.Forms;
+using RestSharp;
+
+namespace ssmh
+{
+    public partial class Form1 : ModernForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void modernButton2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd=new FolderBrowserDialog();
+            fbd.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            fbd.ShowDialog();
+            modernTextBox2.Text = fbd.SelectedPath;
+        }
+
+        private async void modernButton1_Click(object sender, EventArgs e)
+        {
+            items.Clear();
+            if (!modernTextBox1.Text.StartsWith("http://www.wnacg.com/photos"))
+            {
+                MessageBox.Show("uri error");
+                return;
+            }
+            modernButton3.Enabled = false;
+            title = string.Empty;
+            await geteachpage(modernTextBox1.Text);
+            modernButton3.Enabled = true;
+        }
+        BindingList<Item> items=new BindingList<Item>();
+        private string title = "";
+
+        private void getregex(string text)
+        {
+            var matches = Regex.Matches(text,
+                "<div class=\"pic_box\"><a href=\"(?<grp0>[^\"]+)\">");
+            HashSet<string> its = new HashSet<string>();
+            foreach (Match match in matches)
+            {
+                its.Add(match.Groups["grp0"].Value);
+            }
+            foreach (string url in its)
+            {
+                Item i = geteachimg(url);
+                this.Invoke(new Action((() =>
+                {
+
+                    items.Add(i);
+
+
+                })));
+
+
+            }
+
+        }
+
+        private Item geteachimg(string text)
+        {
+            RestClient rc = new RestClient(new Uri("http://www.wnacg.com" + text));
+            RestRequest req = new RestRequest();
+            req.Method = Method.GET;
+            IRestResponse res = rc.Execute(req);
+            var m =
+                Regex.Match(res.Content,
+                    "<img id=\"picarea\" class=\"photo\" alt=\"(?<grp0>[^\\D]+)\" src=\"(?<grp1>[^\"]+)\" />");
+            Item i=new Item();
+            i.Title = m.Groups["grp0"].Value;
+            i.Url = "http://www.wnacg.com" + m.Groups["grp1"].Value;
+            return i;
+
+        }
+        private async Task<bool> geteachpage(string url)
+        {
+            return await Task.Run((async () =>
+            {
+                
+
+                RestClient rc = new RestClient(new Uri(url));
+                RestRequest req = new RestRequest();
+                req.Method = Method.GET;
+                IRestResponse res = rc.Execute(req);
+                getregex(res.Content);
+                this.Invoke(new Action((() =>
+                {
+                    if (string.IsNullOrEmpty(title))
+                    {
+                        title = Regex.Match(res.Content, "<h2>(?<grp0>.+?)</h2>").Groups["grp0"].Value;
+                    }
+
+                })));
+                var match = Regex.Match(res.Content, "<a href=\"(?<grp0>[^\"]+)\">後頁");
+                if (match.Success)
+                {
+                    await geteachpage("http://www.wnacg.com" + match.Groups["grp0"].Value);
+                }
+                return true;
+            }));
+            
+        }
+
+        private async void modernButton3_Click(object sender, EventArgs e)
+        {
+            modernButton1.Enabled = false;
+            modernButton2.Enabled = false;
+            modernButton3.Enabled = false;
+           await Task.Run((() =>
+           {
+               string t=null;
+                this.Invoke(new Action((() =>
+                {
+                    t = title;
+                })));
+             
+               StringBuilder rBuilder = new StringBuilder(t);
+               foreach (char rInvalidChar in Path.GetInvalidFileNameChars())
+                   rBuilder.Replace(rInvalidChar.ToString(), string.Empty);
+
+               string outpath = modernTextBox2.Text+"\\" + rBuilder + "\\";
+               Directory.CreateDirectory(outpath);
+                foreach (Item item in items)
+                {
+                   this.Invoke(new Action((() =>
+                   {
+                       var currentrow = modernDataGridView1.Rows[items.IndexOf(item)];
+                       modernDataGridView1.ClearSelection();
+                       currentrow.Selected = true;
+                       modernDataGridView1.CurrentCell = currentrow.Cells[0];
+                       item.状态 = "下载中";
+                   })));
+                   RestClient rc = new RestClient(new Uri(item.Url));
+                    RestRequest req = new RestRequest();
+                    req.Method = Method.GET;
+                    IRestResponse res = rc.Execute(req);
+                    File.WriteAllBytes(outpath + item.Title.Replace(" ", "") + ".jpg", res.RawBytes);
+                    this.Invoke(new Action((() =>
+                    {
+                        item.状态 = "下载完成";
+                    })));
+                    
+                }
+            }));
+            modernDataGridView1.Refresh();
+            ModernMessageBox.Show("OK", "Message");
+            modernButton1.Enabled = true;
+            modernButton2.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            modernDataGridView1.DataSource = items;
+            modernTextBox2.Text= Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            modernDataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            modernDataGridView1.Columns[1].AutoSizeMode=DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        private void modernTextBox1_Click(object sender, EventArgs e)
+        {
+            modernTextBox1.Text = "";
+        }
+    }
+
+    public class Item
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string 状态 { get; set; }
+    }
+}
